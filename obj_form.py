30f7ab17f4b037a239995743cd639bbd85df5f29
Ethan Nguyen <ethan.nguyen@ou.edu>
2024-02-21 13:36:46 -0600

    create and setup obj_form

---
+++ obj_form.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+class ObjForm:
+    def __init__(self, master):
+        self.master = master
+        master.title("Fitts Law Game")
+        master.geometry('1200x900')
+        
