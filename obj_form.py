d46df2b9cdeb080a5219a8949d6a5cc3c7ed235b
Ethan Nguyen <ethan.nguyen@ou.edu>
2024-02-28 14:45:21 -0600

    made the baseline coding of the fitts law

--- obj_form.py
+++ obj_form.py
@@ -17,8 +17,18 @@ class ObjForm:
         back_btn['font'] = myFont
         back_btn.pack(side=tk.TOP, anchor=tk.NE, padx=10,pady=15)
         
-        self.draw_boxes()
+        self.elapsed_time_label = tk.Label(master, text="", font = myFont)
+        self.elapsed_time_label.pack(pady=10)
         
+        self.clicks_label = tk.Label(master, text="", font=myFont)
+        self.clicks_label.pack(pady=10)
+        
+        self.boxclick = 0
+        self.left_press_count = 0
+        self.right_press_count = 0
+        
+        self.draw_boxes()
+
         #Prevent program from not completely exiting out
         master.protocol("WM_DELETE_WINDOW", self.back_mm)
     
@@ -51,3 +61,42 @@ class ObjForm:
         self.right_box = tk.Label(self.master, text="Right Box", bg="red", width=box_width, height=box_height)
         self.right_box.place(x=right_box_x1, y=right_box_y1)
         self.right_box.bind('<Button-1>', lambda event: self.record_right_press())
+    
+    def start_stopwatch_left(self):
+        if self.left_press_count == 0:
+            self.stopwatch.startTime()
+            self.timer()
+        self.left_press_count += 1
+        self.record_clicks()
+        
+            
+    def record_right_press(self):
+        self.right_press_count += 1
+        self.record_clicks()
+        
+        if self.right_press_count >= 5:
+            self.stopwatch.stopTime()
+            recorded_time = self.stopwatch.elapsed_time()
+            self.display_recorded_time(recorded_time)
+            self.reset_timer()
+            
+    def record_clicks(self):
+        self.boxclick += 1
+        self.clicks_label.config(text="Clicks: {:}" .format(self.boxclick))
+        
+    def timer(self):
+        if self.stopwatch.start_time:
+            elapsed_time = time.time() - self.stopwatch.start_time
+            self.elapsed_time_label.config(text="Time: {:.2f} seconds." .format(elapsed_time))
+            self.master.after(100, self.timer)
+            
+    def reset_timer(self):
+        self.left_press_count = 0
+        self.right_press_count = 0
+        self.boxclick = 0
+        self.stopwatch.reset()
+        self.elapsed_time_label.config(text="")
+        self.clicks_label.config(text="Clicks: 0")
+
+    def display_recorded_time(self, recorded_time):
+        print("Recorded Time:", recorded_time)
