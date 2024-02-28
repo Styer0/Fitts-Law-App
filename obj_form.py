08b45dabaa65476fe321fd5b2e5677cc3bfcf9f5
Ethan Nguyen <ethan.nguyen@ou.edu>
2024-02-28 13:36:26 -0600

    added boxes for the fitts law experiment

--- obj_form.py
+++ obj_form.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+import time
+from stopwatch import Stopwatch
 
 myFont = ("Consolas", 13)
 class ObjForm:
     def __init__(self, master, main_menu):
         self.master = master
         self.main_menu = main_menu
+        self.stopwatch = Stopwatch()
+        
         master.title("Fitts Law Game")
         master.geometry('1200x900')
 
@@ -13,6 +17,8 @@ class ObjForm:
         back_btn['font'] = myFont
         back_btn.pack(side=tk.TOP, anchor=tk.NE, padx=10,pady=15)
         
+        self.draw_boxes()
+        
         #Prevent program from not completely exiting out
         master.protocol("WM_DELETE_WINDOW", self.back_mm)
     
@@ -20,3 +26,28 @@ class ObjForm:
     def back_mm(self):
         self.master.destroy()
         self.main_menu.master.deiconify()
+    
+    def draw_boxes(self):
+        box_width = 50
+        box_height = 25
+        
+        #Left box coordinates
+        left_box_x1 = 50
+        left_box_y1 = 300
+        left_box_x2 = left_box_x1 + box_width
+        left_box_y2 = left_box_y1 + box_height
+        
+        right_box_x1 = 1500
+        right_box_y1 = 300
+        right_box_x2 = right_box_x1 + box_width
+        right_box_y2 = right_box_y1 + box_height
+        
+        # Draw left box
+        self.left_box = tk.Label(self.master, text="Left Box", bg="blue", width=box_width, height=box_height)
+        self.left_box.place(x=left_box_x1, y=left_box_y1)
+        self.left_box.bind('<Button-1>', lambda event: self.start_stopwatch_left())
+
+        # Draw right box
+        self.right_box = tk.Label(self.master, text="Right Box", bg="red", width=box_width, height=box_height)
+        self.right_box.place(x=right_box_x1, y=right_box_y1)
+        self.right_box.bind('<Button-1>', lambda event: self.record_right_press())
